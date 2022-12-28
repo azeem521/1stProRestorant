99b7a3ed90afd0117a2ddabd19e26f6d3e0c760d
Mohd <azeemsre1@gmail.com>
2022-12-28 11:48:43 +0530

    SummaryAdded

--- restorant/src/App.js
+++ restorant/src/App.js
@@ -1,10 +1,14 @@
 import Header from "./component/Layout/Header";
+import Summary from "./component/Meals/Summary";
 
 function App() {
   return (
     <div>
       <h2>Let's get started!</h2>
       <Header />
+      <main>
+        <Summary />
+      </main>
     </div>
   );
 }

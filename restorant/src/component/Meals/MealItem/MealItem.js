39545a0398900e125b777d77fc3ef5ab70a6a3c9
Mohd <azeemsre1@gmail.com>
2022-12-30 10:51:43 +0530

    FormAdded

--- restorant/src/component/Meals/MealItem/MealItem.js
+++ restorant/src/component/Meals/MealItem/MealItem.js
@@ -1,6 +1,7 @@
 import React from 'react'
 
 import classes from './MealItem.module.css'
+import MealItemForm from './MealItemForm'
 
 const MealItem = (props) => {
 
@@ -15,7 +16,7 @@ const MealItem = (props) => {
         </div>
         <div>
           
-          
+          <MealItemForm />
         </div>
     </li>
   )
